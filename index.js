41433456dc13b8035cd9bf0dbca542a8b14a1e91
Daniel Magers <daniel.magers@gmail.com>
2022-07-20 05:39:16 -0500

    commit tree 6f15093f47302f8b34488ab0e4a30be081627485

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-function addingEventListener() {
-    const input = document.getElementById('button');
+// const input = document.getElementById('button');
+// input.addEventListener('click', function(){
+//   alert('I was clicked!');
+// });
 
-    function clickAlert() {
-      alert('I was clicked!');
-    }
-    
-    input.addEventListener('click', clickAlert);
+const input = document.getElementById('button');
+
+function addingEventListener() {
+
+function clickAlert(){
+  alert('I was clicked!')
+}
+
+input.addEventListener('click', clickAlert)
 }
